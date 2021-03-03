7ab618c6585e65869c2fe89caac4ed342f7fde96
Brayden Werner <braydenwerner0@gmail.com>
2021-03-03 11:10:01 -0800

    material ui

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
+import { Button } from '@material-ui/core'
+
 export default function Slider() {
-  return <div>Test</div>
+  return (
+    <div>
+      <Button color="primary">Test Button</Button>
+    </div>
+  )
 }
